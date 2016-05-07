d15f1ad7fae3c85558e258af155ac6f90cbf98e1
greenbreakfast <lazardemin@gmail.com>
2016-05-06 23:24:05 -0400

    added tool to perform remote compile

---
+++ tools/remote_compile.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+## define the remote server and package
+server="lazar@build.onion.io"
+remotePath="/home/lazar/OpenWRT-Buildroot/openwrt/dl"
+package="onion-helper"
+
+localPath="../$package"
+
+## upload project to remote server
+cmd="rsync -va --progress $localPath $server:$remotePath"
+echo "$cmd"
+eval "$cmd"
+
+
+## create a tar from the file, run the compile
+cmd="ssh $server \"cd $remotePath && tar -zcvf $package.tar.gz $package && cd .. && make package/feeds/onion/$package/compile V=99\""
+echo "$cmd"
+eval "$cmd"
+
